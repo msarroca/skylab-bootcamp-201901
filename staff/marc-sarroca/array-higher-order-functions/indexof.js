23c49408f919f58a0818a9166761ab92a2654e91
Marc Sarroca Diaz <msarroca@Marcs-MacBook-Pro.local>
2019-01-15 16:12:30 +0100

    funtions

--- staff/marc-sarroca/array-higher-order-functions/indexof.js
+++ staff/marc-sarroca/array-higher-order-functions/indexof.js
@@ -1,10 +1,25 @@
+/**
+ * Abstraction of fill.
+ *
+ * Fills an array from one position to other.
+ *
+ * @param {Array} array
+ * @param {*} value
+ * @throws {Error} - If too many arguments (> 4)
+ * @throws {TypeError} - If array is not an array
+ */
+
 function indexOf(arr, value) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === value) {
       return i;
     }
+    if (i === arr.length - 1) {
+      return -1;
+    }
   }
 }
+
 var array = [2, 4, 5, 6];
-var res = indexOf(array, 5);
+var res = indexOf(array, 1);
 console.log(res);
