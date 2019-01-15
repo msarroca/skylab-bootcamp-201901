158dd86607b12b69e17c7ff5b7888bf85972eddf
Marc Sarroca Diaz <msarroca@Marcs-MacBook-Pro.local>
2019-01-15 14:04:35 +0100

    functions

--- staff/marc-sarroca/array-higher-order-functions/fill.js
+++ staff/marc-sarroca/array-higher-order-functions/fill.js
@@ -1,8 +1,27 @@
-function fill(arr, value, start, end) {
-  for (var i = start; i < end; i++) {
-    arr[i] = value;
-  }
+/**
+ * Abstraction of fill.
+ * 
+ * Fills an array from one position to other.
+ * 
+ * @param {Array} array 
+ * @param {*} value 
+ * @param {number} start 
+ * @param {number} end 
+ * 
+ * @throws {Error} - If too many arguments (> 4)
+ * @throws {TypeError} - If array is not an array
+ */
+function fill(array, value, start, end) {
+    if (arguments.length > 4) throw Error('too many arguments');
+
+    if (!(array instanceof Array))
+        throw new TypeError(array + ' is not an array');
+
+    start = start === undefined ? 0 : (start < 0 ? array.length + start : start);
+    end = end === undefined ? array.length : (end < 0 ? array.length + end : end);
+
+    for (var i = start; i < end; i++)
+        array[i] = value;
+
+    return array;
 }
-var array = [2, 4, 5, 6];
-fill(array, 5);
-console.log(array);
