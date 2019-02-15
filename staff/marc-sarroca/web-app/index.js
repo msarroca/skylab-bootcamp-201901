b7ef5fbf8b1096bb951b096f38cc7ca83f046fed
Marc Sarroca Diaz <msarroca@Marcs-MacBook-Pro.local>
2019-02-15 16:52:30 +0100

    web-app

--- staff/marc-sarroca/web-app/index.js
+++ staff/marc-sarroca/web-app/index.js
@@ -1,133 +1,155 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
-const logic = require('./src/logic')
+const session = require('express-session')
+// const FileStore = require('session-file-store')(session)
+const logicFactory = require('./src/logic-factory')
 
-const { argv: [, , port = 8081] } = process
+const { env: { PORT }, argv: [, , port = PORT || 8080] } = process
+
 const app = express()
-const formBodyParser = bodyParser.urlencoded({ extended: false })
+
+app.use(session({
+    secret: 'a secret phrase',
+    resave: true,
+    saveUninitialized: true,
+    // store: new FileStore({
+    //     path: './.sessions'
+    // })
+}))
 
 app.use(express.static('public'))
 
-let feedback = ''
+app.set('view engine', 'pug')
+app.set('views', './src/components')
 
-app.get('/register', (req, res) => {
-    res.send(`<html>
+const formBodyParser = bodyParser.urlencoded({ extended: false })
+
+function pullFeedback(req) {
+    const { session: { feedback } } = req
+
+    req.session.feedback = null
+
+    return feedback
+}
+
+function renderPage(content) {
+    return `<html>
 <head>
-    <title>HELLO WORLD</title>
+    <title>HELLO WORLD!</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
-    <h1>HELLO WORLD</h1>
-    <section class="register">
-        <h2>Register</h2>
-        <form method="POST" action="/register">
-        <input name="name" type="text" placeholder="name" required>
-        <input name="surname" type="text" placeholder="surname" required>
-        <input name="email" type="email" placeholder="email" required>
-        <input name="password" type="password" placeholder="password" required>
-        <input name="passwordConfirm" type="password" placeholder="confirm password" required>
-        <button type="submit">Register</button>
-        </form>
-        ${feedback ? `<section class="feedback feedback--warn">
-            ${feedback}
-        </section>` : ''}
-    </section>
+<body class="main">
+    <h1>HELLO WORLD! 🤡</h1>
+    ${content}
 </body>
-</html>`)
+</html>`
+}
+
+app.get('/', (req, res) => {
+    res.render('landing')
+})
+
+app.get('/register', (req, res) => {
+    const logic = logicFactory.create(req)
+
+    if (logic.isUserLoggedIn) {
+        res.redirect('/home')
+    } else {
+        const feedback = pullFeedback(req)
+
+        res.render('register', { feedback })
+    }
 })
 
 app.post('/register', formBodyParser, (req, res) => {
     const { body: { name, surname, email, password, passwordConfirm } } = req
 
+    const logic = logicFactory.create(req)
+
     try {
         logic.registerUser(name, surname, email, password, passwordConfirm)
-            .then(() => res.send(`<html>
-<head>
-    <title>HELLO WORLD</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    <h1>HELLO WORLD</h1>
-    <section class="register">
-        <h2>Registration confirmation</h2>
-        Ok, user <strong>${email}</strong> successfully registered, please proceed to <a href="/login">login</a>.
-        </form>
-    </section>
-</body>
-</html>`))
+            .then(() => res.render('register-confirm', { email }))
             .catch(({ message }) => {
-                feedback = message
+                req.session.feedback = message
 
                 res.redirect('/register')
             })
     } catch ({ message }) {
-        feedback = message
+        req.session.feedback = message
 
         res.redirect('/register')
     }
 })
 
 app.get('/login', (req, res) => {
-    res.send(`<html>
-<head>
-    <title>HELLO WORLD</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    <h1>HELLO WORLD</h1>
-    <section class="login">
-        <h2>Login</h2>
-        <form method="POST" action="/login">
-      
-        <input name="email" type="email" placeholder="email" required>
-        <input name="password" type="password" placeholder="password" required>
-        <button type="submit">Login</button>
-        </form>
-        ${feedback ? `<section class="feedback feedback--warn">
-            ${feedback}
-        </section>` : ''}
-    </section>
-</body>
-</html>`)
+    const logic = logicFactory.create(req)
+
+    if (logic.isUserLoggedIn) {
+        res.redirect('/home')
+    } else {
+        const feedback = pullFeedback(req)
+
+        res.render('login', { feedback })
+    }
 })
 
 app.post('/login', formBodyParser, (req, res) => {
     const { body: { email, password } } = req
 
+    const logic = logicFactory.create(req)
+
     try {
         logic.logInUser(email, password)
             .then(() => res.redirect('/home'))
             .catch(({ message }) => {
-                feedback = message
+                req.session.feedback = message
 
                 res.redirect('/login')
             })
     } catch ({ message }) {
-        feedback = message
+        req.session.feedback = message
 
         res.redirect('/login')
     }
 })
 
 app.get('/home', (req, res) => {
-    if (logic.isUserLoggedIn) {
-        res.send(`<html>
- <head>
-    <title>HELLO WORLD</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
- </head>
-    <body>
-    <h1>HELLO MAMABICHO</h1>
-    </body>
-</html>`)
+    try {
+        const { session: { feedback } } = req
 
+        const logic = logicFactory.create(req)
 
-    } else {
-        res.redirect('/login')
+        if (logic.isUserLoggedIn)
+            logic.retrieveUser()
+                .then(({ name }) => res.render('home', { feedback, name }))
+                .catch(({ message }) => {
+                    req.session.feedback = message
+
+                    res.redirect('/home')
+                })
+        else res.redirect('/login')
+    } catch ({ message }) {
+        req.session.feedback = message
+
+        res.redirect('/home')
     }
 })
 
-// TODO get and post login
-// TODO get home (must control user is logged in)
+app.post('/logout', (req, res) => {
+    const logic = logicFactory.create(req)
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+    logic.logOutUser()
+
+    res.redirect('/')
+})
+
+app.get('*', (req, res) => res.send(404, renderPage(`<section class="not-found">
+        <h2>NOT FOUND</h2>
+
+        Go <a href="/">Home</a>
+    </section>`)))
+
+
+app.listen(port, () => console.log(`server running on port ${port}`))
--- staff/marc-sarroca/web-app/src/logic/index.spec.js
+++ staff/marc-sarroca/web-app/src/logic/index.spec.js
@@ -1,14 +1,15 @@
 'use strict'
 const { expect } = require('chai')
-const logic = require('.')
+const Logic = require('.')
 
-describe('logic', () => {
+describe('Logic', () => {
     describe('register user', () => {
         const name = 'Manuel'
         const surname = 'Barzi'
         const email = `manuelbarzi@mail.com-${Math.random()}`
         const password = '123'
         const passwordConfirm = password
+        const logic = new Logic
 
         it('should succeed on valid data', () =>
             logic.registerUser(name, surname, email, password, passwordConfirm)
@@ -148,222 +149,91 @@ describe('logic', () => {
                 logic.registerUser(name, surname, email, password, password)
             }).to.throw(Error, 'surname cannot be empty')
         })
-
-        it('should fail on empty email', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = ''
-            const password = '123'
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(Error, 'email cannot be empty')
-        })
-
-        it('should fail on udefined email', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = undefined
-            const password = '123'
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(Error, 'is not a string')
-        })
-
-        it('should fail on numeric email', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = 123
-            const password = '123'
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(TypeError, email + ' is not a string')
-        })
-
-        it('should fail on boolean email', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = true
-            const password = '123'
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(TypeError, email + ' is not a string')
-        })
-
-        it('should fail on object email', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = {}
-            const password = '123'
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(TypeError, email + ' is not a string')
-        })
-
-        it('should fail on array email', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = []
-            const password = '123'
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(TypeError, email + ' is not a string')
-
-        })
-
-        it('should fail on empty password', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = 'manuelbarzi@mail.com'
-            const password = ''
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(Error, 'email cannot be empty')
-        })
-
-        it('should fail on udefined password', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = 'manuelbarzi@mail.com'
-            const password = undefined
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(Error, 'is not a string')
-        })
-
-        it('should fail on numeric password', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = 'manuelbarzi@mail.com'
-            const password = 123
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(TypeError, email + ' is not a string')
-        })
-
-        it('should fail on boolean password', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = 'manuelbarzi@mail.com'
-            const password = true
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(TypeError, email + ' is not a string')
-        })
-
-        it('should fail on object password', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = 'manuelbarzi@mail.com'
-            const password = {}
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(TypeError, email + ' is not a string')
-        })
-
-        it('should fail on array email', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = 'manuelbarzi@mail.com'
-            const password = []
-
-            expect(() => {
-                logic.registerUser(name, surname, email, password, password)
-            }).to.throw(TypeError, email + ' is not a string')
-
-        })
-
-        describe('log in user', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = `manuelbarzi@mail.com-${Math.random()}`
-            const password = '123'
-            const passwordConfirm = password
-
-            beforeEach(() =>
-                logic.registerUser(name, surname, email, password, passwordConfirm)
-            )
-
-            it('should succeed on correct credentials', () =>
-                logic.logInUser(email, password)
-                    .then(() => {
-                        expect(logic.__userId__).to.exist
-                        expect(logic.__userApiToken__).to.exist
-                    })
-            )
-        })
-
-        describe('check user is logged in', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = `manuelbarzi@mail.com-${Math.random()}`
-            const password = '123'
-            const passwordConfirm = password
-
-            beforeEach(() =>
-                logic.registerUser(name, surname, email, password, passwordConfirm)
-            )
-
-            it('should succeed on correct credentials', () =>
-                logic.logInUser(email, password)
-                    .then(() => expect(logic.isUserLoggedIn).to.be.true)
-            )
-        })
-
-        describe('log out user', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = `manuelbarzi@mail.com-${Math.random()}`
-            const password = '123'
-            const passwordConfirm = password
-
-            beforeEach(() =>
-                logic.registerUser(name, surname, email, password, passwordConfirm)
-                    .then(() => logic.logInUser(email, password))
-            )
-
-            it('should succeed on correct credentials', () => {
-                logic.logOutUser()
-
-                expect(logic.__userId__).to.be.null
-                expect(logic.__userId__).to.be.null
-            })
-        })
-
-        describe('retrieve user', () => {
-            const name = 'Manuel'
-            const surname = 'Barzi'
-            const email = `manuelbarzi@mail.com-${Math.random()}`
-            const password = '123'
-            const passwordConfirm = password
-
-            beforeEach(() =>
-                logic.registerUser(name, surname, email, password, passwordConfirm)
-                    .then(() => logic.logInUser(email, password))
-            )
-
-            it('should succeed on correct credentials', () =>
-                logic.retrieveUser()
-                    .then(user => {
-                        expect(user.id).to.equal(logic.__userId__)
-                        expect(user.name).to.equal(name)
-                        expect(user.surname).to.equal(surname)
-                        expect(user.email).to.equal(email)
-                    })
-            )
-        })
-
-        // TODO updateUser and removeUser
     })
 
+    describe('log in user', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const email = `manuelbarzi@mail.com-${Math.random()}`
+        const password = '123'
+        const passwordConfirm = password
+        const logic = new Logic
+
+        beforeEach(() =>
+            logic.registerUser(name, surname, email, password, passwordConfirm)
+        )
+
+        it('should succeed on correct credentials', () =>
+            logic.logInUser(email, password)
+                .then(() => {
+                    expect(logic.__storage__.userId).to.exist
+                    expect(logic.__storage__.userApiToken).to.exist
+                })
+        )
+    })
+
+    describe('check user is logged in', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const email = `manuelbarzi@mail.com-${Math.random()}`
+        const password = '123'
+        const passwordConfirm = password
+        const logic = new Logic
+
+        beforeEach(() =>
+            logic.registerUser(name, surname, email, password, passwordConfirm)
+        )
+
+        it('should succeed on correct credentials', () =>
+            logic.logInUser(email, password)
+                .then(() => expect(logic.isUserLoggedIn).to.be.true)
+        )
+    })
+
+    describe('log out user', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const email = `manuelbarzi@mail.com-${Math.random()}`
+        const password = '123'
+        const passwordConfirm = password
+        const logic = new Logic
+
+        beforeEach(() =>
+            logic.registerUser(name, surname, email, password, passwordConfirm)
+                .then(() => logic.logInUser(email, password))
+        )
+
+        it('should succeed on correct credentials', () => {
+            logic.logOutUser()
+
+            expect(logic.__storage__.userId).to.be.null
+            expect(logic.__storage__.userId).to.be.null
+        })
+    })
+
+    describe('retrieve user', () => {
+        const name = 'Manuel'
+        const surname = 'Barzi'
+        const email = `manuelbarzi@mail.com-${Math.random()}`
+        const password = '123'
+        const passwordConfirm = password
+        const logic = new Logic
+
+        beforeEach(() =>
+            logic.registerUser(name, surname, email, password, passwordConfirm)
+                .then(() => logic.logInUser(email, password))
+        )
+
+        it('should succeed on correct credentials', () =>
+            logic.retrieveUser()
+                .then(user => {
+                    expect(user.id).to.equal(logic.__storage__.userId)
+                    expect(user.name).to.equal(name)
+                    expect(user.surname).to.equal(surname)
+                    expect(user.email).to.equal(email)
+                })
+        )
+    })
+
+    // TODO updateUser and removeUser
 })
